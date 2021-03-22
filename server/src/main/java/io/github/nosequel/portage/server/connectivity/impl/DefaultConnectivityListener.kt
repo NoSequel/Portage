b11cf1153c70492d7bd91d36ecfb89f59714a86e
NoSequel <nosequel@protonmail.com>
2021-03-22 16:34:24 +0000

    add player's rank color to connectivity messages

--- server/src/main/java/io/github/nosequel/portage/server/connectivity/impl/DefaultConnectivityListener.kt
+++ server/src/main/java/io/github/nosequel/portage/server/connectivity/impl/DefaultConnectivityListener.kt
@@ -14,21 +14,21 @@ class DefaultConnectivityListener : ConnectivityListener {
      * Handle a player connecting to the network
      */
     override fun handleConnect(session: Session,  server: Server) {
-        this.broadcastStaffMessage("${ChatColor.BLUE}[Staff] ${ChatColor.AQUA}${session.name} ${ChatColor.GREEN}joined ${ChatColor.AQUA}the network (from ${server.name})")
+        this.broadcastStaffMessage("${ChatColor.BLUE}[Staff] ${ChatColor.AQUA}${session.getDisplayName()} ${ChatColor.GREEN}joined ${ChatColor.AQUA}the network (from ${server.name})")
     }
 
     /**
      * Handle a player disconnecting from the network
      */
     override fun handleDisconnect(session: Session, server: Server) {
-        this.broadcastStaffMessage("${ChatColor.BLUE}[Staff] ${ChatColor.AQUA}${session.name} ${ChatColor.RED}left ${ChatColor.AQUA}the network (from ${server.name})")
+        this.broadcastStaffMessage("${ChatColor.BLUE}[Staff] ${ChatColor.AQUA}${session.getDisplayName()} ${ChatColor.RED}left ${ChatColor.AQUA}the network (from ${server.name})")
     }
 
     /**
      * Handle a player switching servers on the network
      */
     override fun handleSwitch(session: Session, serverTo: Server, serverFrom: Server) {
-        this.broadcastStaffMessage("${ChatColor.BLUE}[Staff] ${ChatColor.AQUA}${session.name}${ChatColor.GREEN} joined ${ChatColor.AQUA}${serverTo.name} (from ${serverFrom.name})")
+        this.broadcastStaffMessage("${ChatColor.BLUE}[Staff] ${ChatColor.AQUA}${session.getDisplayName()}${ChatColor.GREEN} joined ${ChatColor.AQUA}${serverTo.name} (from ${serverFrom.name})")
     }
 
     /**
--- server/src/main/java/io/github/nosequel/portage/server/session/Session.kt
+++ server/src/main/java/io/github/nosequel/portage/server/session/Session.kt
@@ -1,5 +1,8 @@
 package io.github.nosequel.portage.server.session
 
+import io.github.nosequel.portage.bukkit.util.ColorUtils
+import io.github.nosequel.portage.core.grant.GrantHandler
+import io.github.nosequel.portage.core.handler.HandlerManager
 import io.github.nosequel.portage.server.`object`.Server
 import io.github.nosequel.portage.server.session.task.SessionChecker
 import java.util.UUID
@@ -28,5 +31,15 @@ class Session(val uuid: UUID, val name: String, var server: Server) {
         this.lastLogin = System.currentTimeMillis()
         this.lastActivity = SessionActivity.JOIN
         this.server = server
+    }
+
+    /**
+     * Get the display name of a session
+     */
+    fun getDisplayName(): String {
+        return "${
+            ColorUtils.getRankColor(HandlerManager.instance.findOrThrow(GrantHandler::class.java).findGrant(uuid)
+                .findRank())
+        }$name"
     }
 }
